1caa800dd5cd33b5aeddcdcee068132276a98b54
Akinpelu Oluwasegun <whizz_fgcfr@yahoo.co.uk>
2021-07-01 20:39:32 +0100

    migrate db

---
+++ Src/Migrations/20210701193849_test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aduaba.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Cards",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Cards");
+        }
+    }
+}
